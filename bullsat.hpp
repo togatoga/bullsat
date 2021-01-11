b266039c014f531f096294238358dcb562c91ffa
togatoga <togasakitogatoga@gmail.com>
2021-01-12 00:35:40 +0900

    Add enqueue and eval

--- bullsat.hpp
+++ bullsat.hpp
@@ -1,9 +1,99 @@
 #ifndef BULLSAT_HPP
 #define BULLSAT_HPP
+#include <cassert>
 #include <iostream>
+#include <optional>
+#include <unordered_map>
+#include <vector>
 
 namespace bullsat {
-class Solver {};
+
+// definitions
+enum class Status { Sat, Unsat, Unknown };
+enum class LitBool { True, False, Undefine };
+using Var = int;
+struct Lit;
+using ClauseIdx = int;
+using Clause = std::vector<Lit>;
+
+// x is
+// even: positive x0 (0 -> x0, 2 -> x1)
+// odd: negative !x0 (1 -> !x0, 3 -> !x1)
+struct Lit {
+  Lit() = default;
+  Lit(const Lit &lit) = default;
+  // 0-index
+  // Lit(0, true) means x0
+  // Lit(0, false) means !x0
+  Lit(Var v, bool positive) {
+    assert(v >= 0);
+    x = positive ? 2 * v : 2 * v + 1;
+  }
+  bool operator==(Lit lit) const { return x == lit.x; }
+  bool operator!=(Lit lit) const { return x != lit.x; }
+  bool operator<(Lit lit) const { return x < lit.x; }
+  bool pos() const { return !neg(); }
+  bool neg() const { return x & 1; }
+  Var var() const { return x >> 1; }
+  size_t vidx() const { return static_cast<size_t>(var()); }
+  size_t lidx() const { return static_cast<size_t>(x); }
+  int x;
+};
+
+// ~x0 = !x0
+inline Lit operator~(Lit p) {
+  Lit q(p);
+  q.x ^= 1;
+  return q;
+}
+std::ostream &operator<<(std::ostream &os, const Lit &lit) {
+  os << (lit.neg() ? "!x" : "x") << lit.var();
+  return os;
+}
+
+class Solver {
+public:
+  Solver(size_t variable_num) : que_head(0) {
+    assings.resize(variable_num);
+    watchers.resize(2 * variable_num);
+    reasons.resize(variable_num);
+    levels.resize(variable_num);
+    que.resize(variable_num);
+  }
+  std::vector<bool> assings;
+  std::optional<Status> status;
+  LitBool eval(Lit lit) {
+    if (!levels[lit.vidx()].has_value()) {
+      return LitBool::Undefine;
+    }
+    if (lit.neg()) {
+      return assings[lit.vidx()] ? LitBool::False : LitBool::True;
+    }
+    return assings[lit.vidx()] ? LitBool::True : LitBool::False;
+  }
+  void enqueue(Lit lit, std::optional<ClauseIdx> reason = std::nullopt) {
+
+    assert(!levels[lit.vidx()].has_value());
+    levels[lit.vidx()] = [&]() {
+      if (que.empty()) {
+        return 0;
+      }
+      Lit last = que.back();
+      return levels[last.vidx()].value_or(0);
+    }();
+    assings[lit.vidx()] = lit.pos() ? true : false;
+    reasons[lit.vidx()] = reason;
+    que.push_back(lit);
+  }
+
+private:
+  std::vector<Clause> clauses;
+  std::vector<std::vector<ClauseIdx>> watchers;
+  std::vector<std::optional<ClauseIdx>> reasons;
+  std::vector<std::optional<int>> levels;
+  std::vector<Lit> que;
+  int que_head;
+};
 } // namespace bullsat
 
 #endif
--- test.cpp
+++ test.cpp
@@ -1,8 +1,60 @@
 #include "bullsat.hpp"
+#include <cassert>
 #include <iostream>
-
 using namespace std;
+using namespace bullsat;
 
+void test_start(const char *funcname) {
+  cerr << "==================== " << funcname
+       << " ==================== " << endl;
+}
+
+void test_lit() {
+  test_start(__func__);
+  Lit x0 = Lit(0, true);   // x0
+  Lit nx0 = Lit(0, false); // !x0
+  assert(x0.lidx() == 0);
+  assert(nx0.lidx() == 1);
+
+  assert(x0.pos());
+  assert(!x0.neg());
+  assert(nx0.neg());
+  assert(x0.pos());
+
+  assert(x0.var() == nx0.var());
+
+  assert(x0 != nx0);
+  assert(~x0 == nx0);
+  assert(x0.lidx() == (~nx0).lidx());
+
+  Lit x1 = Lit(1, true); // x1
+  assert(x1.lidx() == 2);
+  assert(x0 < x1);
+  assert(!x1.neg());
+  cerr << x0 << " " << nx0 << " " << x1 << endl;
+}
+
+void test_enqueue_and_eval() {
+  test_start(__func__);
+  {
+    size_t n = 10;
+    Solver solver = Solver(n);
+    for (size_t i = 0; i < n; i++) {
+      Var x = static_cast<Var>(i);
+      assert(solver.eval(Lit(x, true)) == LitBool::Undefine);
+      assert(solver.eval(Lit(x, false)) == LitBool::Undefine);
+    }
+    Lit x0 = Lit(0, true);
+    Lit nx0 = ~x0;
+    Lit x1 = Lit(1, true);
+    solver.enqueue(x0);
+    assert(solver.eval(x0) == LitBool::True);
+    assert(solver.eval(nx0) == LitBool::False);
+    assert(solver.eval(x1) == LitBool::Undefine);
+  }
+}
 int main() {
   cerr << "===================== test ===================== " << endl;
+  test_lit();
+  test_enqueue_and_eval();
 }
