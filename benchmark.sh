e36aed5cd1fae098565fa3ea563b98f4841e097b
togatoga <togasakitogatoga@gmail.com>
2021-01-14 18:35:29 +0900

    Update benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -4,8 +4,11 @@ set -u
 SOLVER=$1
 TIMELIMIT=$2
 
+sat=0
+unsat=0
+unknown=0
 total_ms=0
-echo "Tim Limit... ${TIMELIMIT} s"
+echo "Time Limit... ${TIMELIMIT} s"
 for file in `find cnf/benchmark/*/*.cnf -type f`; do
 
     cnf=`basename $file`
@@ -17,9 +20,17 @@ for file in `find cnf/benchmark/*/*.cnf -type f`; do
     timeout ${TIMELIMIT}s ${SOLVER} $file $result
     end_ms=`date +%s%3N`
     status=`head -n 1 $result`
+    if [ ${status} = "SAT" ]; then
+        sat=`expr $sat + 1`
+    elif [ ${status} = "UNSAT" ]; then
+        unsat=`expr $unsat + 1`
+    else
+        unknown=`expr $unsat + 1`
+    fi
     elasped_ms=`expr $end_ms - $start_ms`
     total_ms=`expr $total_ms + $elasped_ms`
     echo "Status: ${status} Time: ${elasped_ms} ms"
 done
 
+echo "SAT: ${sat} UNSAT: ${unsat} UNKNOWN: ${unknown}"
 echo "Total Time: ${total_ms} ms"
